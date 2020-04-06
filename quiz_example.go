e3cc1017a486d660df3094f18f43d73845967ca6
Sathish.py9@gmail.com <pankajs703@gmail.com>
2020-04-06 17:44:07 +0530

    Adding timer to questions

--- quiz_example.go
+++ quiz_example.go
@@ -5,10 +5,12 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 	csvFileName := flag.String("csv", "problems.csv", "csv file for quiz setup")
+	timeLimit := flag.Int("limit", 2, "timer limit for quiz")
 	flag.Parse()
 	file, err := os.Open(*csvFileName)
 	if err != nil {
@@ -20,15 +22,27 @@ func main() {
 		exit("Failed to load file content")
 	}
 	problems := parseLines(lines)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
 	correct := 0
 	for i, prob := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, prob.q)
-		var answer string
-		fmt.Scanf("%s", &answer)
-		if answer == prob.a {
-			fmt.Println("Correct !")
-			correct += 1
+		fmt.Printf("\n Problem #%d: %s = ", i+1, prob.q)
+		answerCh := make(chan string)
+
+		go func() {
+			var answer string
+			fmt.Scanf("%s", &answer)
+			answerCh <- answer
+		}()
+		select {
+		case <-timer.C:
+			fmt.Printf("You answered %d questions correctly \n", correct)
+			return
+		case answer := <-answerCh:
+			if answer == prob.a {
+				fmt.Println("Correct !")
+				correct += 1
+			}
 		}
 	}
 	fmt.Printf("You answered %d questions correctly", correct)
